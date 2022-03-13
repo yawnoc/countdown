e6f7bd6883aac92edce8adfad626011555c8bdc6
Conway <yawnoc@pm.me>
2022-03-13 13:19:28 +0800

    Add unit testing for letters.py is_valid

--- test_letters.py
+++ test_letters.py
@@ -21,6 +21,16 @@ class TestLetters(unittest.TestCase):
     self.assertEqual(letters.normalise_letters('abc'), 'ABC')
     self.assertEqual(letters.normalise_letters('HeRpdERP'), 'HERPDERP')
     self.assertEqual(letters.normalise_letters(' whitespace\t'), 'WHITESPACE')
+  
+  def test_is_valid(self):
+    self.assertTrue(letters.is_valid('A', 'A'))
+    self.assertTrue(letters.is_valid('A', 'AA'))
+    self.assertTrue(letters.is_valid('ABC', 'AABBCCDD'))
+    self.assertTrue(letters.is_valid('ABBCCCDDDD', 'QWERTYDDDDCCCBBAA'))
+    self.assertTrue(letters.is_valid('RADAR', 'RADAR'))
+    self.assertFalse(letters.is_valid('A', 'X'))
+    self.assertFalse(letters.is_valid('AA', 'A'))
+    self.assertFalse(letters.is_valid('RADAR', 'DARAD'))
 
 
 if __name__ == '__main__':
