2baa680c6c29821f4a5cd0aafec2b05ab86fc67c
Conway <yawnoc@pm.me>
2022-03-13 11:48:06 +0800

    Create letters.py that does nothing yet

---
+++ letters.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+# letters.py
+
+Solve a Countdown letters game.
+"""
+
+
+def main():
+  # TODO:
+  # - Parse command line input for input letters and word list
+  # - For each word in word list:
+  #   - If word can be constructed from input letters:
+  #     - Append to results list
+  # - Sort results and cull
+  # - Write to stdout
+  return
+
+
+if __name__ == '__main__':
+  main()
