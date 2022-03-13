08ca655e8a8d2bd359ca094f500e1dd8efdffc65
Conway <yawnoc@pm.me>
2022-03-13 12:45:17 +0800

    Include license notice in Python scripts

--- letters.py
+++ letters.py
@@ -4,6 +4,10 @@
 # letters.py
 
 Solve a Countdown letters game.
+
+Copyright 2022 Conway
+Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+This is free software with NO WARRANTY etc. etc., see LICENSE.
 """
 
 
