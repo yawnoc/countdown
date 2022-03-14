c960b1de37e2c77a7635e07467afb964e10569f8
Conway <yawnoc@pm.me>
2022-03-14 18:56:48 +0800

    Implement numbers.py argument parsing

---
+++ python/numbers.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+# numbers.py
+
+Solve a Countdown numbers game.
+
+Copyright 2022 Conway
+Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+This is free software with NO WARRANTY etc. etc., see LICENSE.
+"""
+
+
+import argparse
+
+
+def parse_command_line_arguments() -> object:
+  
+  MAX_RESULTS_DEFAULT = 15
+  
+  parser = \
+          argparse.ArgumentParser(
+            description='Solve a Countdown numbers game.'
+          )
+  
+  parser.add_argument(
+    'target',
+    metavar='TARGET',
+    type=int,
+    help='target number (positive integer)',
+  )
+  
+  parser.add_argument(
+    'input_numbers',
+    metavar='NUMBER',
+    type=int,
+    nargs='+',
+    help='number that can be used to obtain the target',
+  )
+  
+  parser.add_argument(
+    '-m', dest="max_results_count",
+    metavar='MAX_RESULTS',
+    type=int,
+    default=MAX_RESULTS_DEFAULT,
+    help=f'maximum number of output results (default {MAX_RESULTS_DEFAULT})',
+  )
+  
+  return parser.parse_args()
+
+
+def main():
+  
+  parsed_arguments = parse_command_line_arguments()
+  
+  target = parsed_arguments.target
+  input_numbers = parsed_arguments.input_numbers
+  max_results_count = parsed_arguments.max_results_count
+
+
+if __name__ == '__main__':
+  
+  main()
