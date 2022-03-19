0473e4b0abcac21410a40b352110d5bb91522ff2
Conway <yawnoc@pm.me>
2022-03-19 22:27:35 +0800

    Implement ArgumentParser.addPositionalArgument

--- java/ArgumentParser.java
+++ java/ArgumentParser.java
@@ -8,6 +8,8 @@
   This is free software with NO WARRANTY etc. etc., see LICENSE.
 */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 public class ArgumentParser
@@ -15,8 +17,11 @@ public class ArgumentParser
   public static final Function<String, String> PARSE_UNTO_STRING =
           (final String argumentString) -> argumentString;
   
+  private final List<PositionalArgument> positionalArgumentList;
+  
   public ArgumentParser()
   {
+    positionalArgumentList = new ArrayList<>();
   }
   
   public <T> void addPositionalArgument(
@@ -27,5 +32,42 @@ public class ArgumentParser
     final Function<String, T> parsingFunction
   )
   {
+    positionalArgumentList.add(
+      new PositionalArgument<T>(
+        internalName,
+        displayName,
+        displayHelp,
+        argumentCount,
+        parsingFunction
+      )
+    );
+  }
+  
+  private class Argument
+  {
+  }
+  
+  private class PositionalArgument<T> extends Argument
+  {
+    private final String internalName;
+    private final String displayName;
+    private final String displayHelp;
+    private final int argumentCount;
+    private final Function<String, T> parsingFunction;
+    
+    private PositionalArgument(
+      final String internalName,
+      final String displayName,
+      final String displayHelp,
+      final int argumentCount,
+      final Function<String, T> parsingFunction
+    )
+    {
+      this.internalName = internalName;
+      this.displayName = displayName;
+      this.displayHelp = displayHelp;
+      this.argumentCount = argumentCount;
+      this.parsingFunction = parsingFunction;
+    }
   }
 }
