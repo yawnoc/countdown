412c7d9658136c4c4edfda12b2ab2c16870d9c33
Conway <yawnoc@pm.me>
2022-03-19 16:18:32 +0800

    Create Letters.java that does nothing
    except print the supplied arguments

---
+++ java/Letters.java
@@ -0,0 +1,17 @@
+/*
+  # Letters.java
+  
+  Solve a Countdown letters game.
+  
+  Copyright 2022 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc., see LICENSE.
+*/
+
+public class Letters
+{
+  public static void main(final String[] arguments)
+  {
+    System.out.println(String.join(",", arguments));
+  }
+}
