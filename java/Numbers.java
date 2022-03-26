a51b52ebaa67cbd4b10c400b0b88db3e4ef62f1c
Conway <yawnoc@pm.me>
2022-03-26 17:50:34 +0800

    Make Java numbers class Complexity static

--- java/Numbers.java
+++ java/Numbers.java
@@ -475,7 +475,7 @@ public class Numbers
       return partString;
     }
     
-    public class Complexity
+    public static class Complexity
     {
       private final int mass;
       private final int depth;
