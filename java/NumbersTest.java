2b14a9c0e4b60aa2283ece454b94c9b7f620d51f
Conway <yawnoc@pm.me>
2022-03-27 17:14:57 +0800

    Write unit tests for Java numbers Expression equals

--- java/NumbersTest.java
+++ java/NumbersTest.java
@@ -98,6 +98,58 @@ public class NumbersTest
     assertEquals(_2_a_2_mm_9_s_4_dd_3_m_2_s_5_d_2.getSignsList(), Arrays.asList(1, 1, -1));
   }
   
+  @Test
+  public void expressionEquals_isCorrect()
+  {
+    final Expression _2 = new Expression(2);
+    final Expression _3 = new Expression(3);
+    final Expression _4 = new Expression(4);
+    final Expression _5 = new Expression(5);
+    final Expression _9 = new Expression(9);
+    
+    final Expression _5_a_2 = new Expression(_5, _2, Expression.ADD);
+    final Expression _2_a_5 = new Expression(_2, _5, Expression.ADD);
+    final Expression _3_a_4 = new Expression(_3, _4, Expression.ADD);
+    final Expression _3_m_3 = new Expression(_3, _3, Expression.MULTIPLY);
+    final Expression _9_d_3 = new Expression(_9, _3, Expression.DIVIDE);
+    final Expression _3_d_9 = new Expression(_3, _9, Expression.DIVIDE);
+    assertEquals(_2_a_5, _2_a_5);
+    assertEquals(_2_a_5, _5_a_2);
+    assertNotEquals(_2_a_5, _3_a_4);
+    assertNotEquals(_3_m_3, _9);
+    assertNotEquals(_9_d_3, _3_d_9);
+    
+    final Expression _3_s_4_aaa_9_s_2_aa_5 =
+            new Expression(
+              new Expression(_3, _4, Expression.SUBTRACT),
+              new Expression(new Expression(_9, _2, Expression.SUBTRACT), _5, Expression.ADD),
+              Expression.ADD
+            );
+    final Expression _9_a_5_a_3_s_4_s_2 =
+            new Expression(
+              new Expression(
+                new Expression(
+                  new Expression(
+                    _9,
+                    _5,
+                    Expression.ADD
+                  ),
+                  _3,
+                  Expression.ADD
+                ),
+                _4,
+                Expression.SUBTRACT
+              ),
+              _2,
+              Expression.SUBTRACT
+            );
+    assertEquals(_3_s_4_aaa_9_s_2_aa_5, _9_a_5_a_3_s_4_s_2);
+  
+    final Expression _5_a_3_m_4 = new Expression(_5, new Expression(_3, _4, Expression.MULTIPLY), Expression.ADD);
+    final Expression _4_m_3_a_5 = new Expression(new Expression(_4, _3, Expression.MULTIPLY), _5, Expression.ADD);
+    assertEquals(_5_a_3_m_4, _4_m_3_a_5);
+  }
+  
   /*
     Alias Numbers.Expression as Expression
   */
